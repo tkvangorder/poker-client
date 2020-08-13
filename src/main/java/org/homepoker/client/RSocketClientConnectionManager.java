a1f06bd762006cc31043b84c144a4680faa984b9
Tyler Van Gorder <tyler.vangorder@build.com>
2020-08-12 21:57:51 -0700

    Primitive security

--- src/main/java/org/homepoker/client/RSocketClientConnectionManager.java
+++ src/main/java/org/homepoker/client/RSocketClientConnectionManager.java
@@ -5,24 +5,38 @@ import java.time.Duration;
 import javax.annotation.PreDestroy;
 
 import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.security.rsocket.metadata.SimpleAuthenticationEncoder;
+import org.springframework.security.rsocket.metadata.UsernamePasswordMetadata;
 import org.springframework.shell.Availability;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
 
+import io.rsocket.metadata.WellKnownMimeType;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 public class RSocketClientConnectionManager {
+	private static final MimeType SIMPLE_AUTH = MimeTypeUtils.parseMimeType(WellKnownMimeType.MESSAGE_RSOCKET_AUTHENTICATION.getString());
+	 
 	private final RSocketRequester.Builder rsocketRequesterBuilder;
 	private RSocketRequester rsocketRequester;
-
-	public RSocketClientConnectionManager(RSocketRequester.Builder rsocketRequesterBuilder) {
+	private final RSocketStrategies rsocketStrategies;
+	
+	public RSocketClientConnectionManager(RSocketRequester.Builder rsocketRequesterBuilder, RSocketStrategies rsocketStrategies) {
 		this.rsocketRequesterBuilder = rsocketRequesterBuilder;
+		this.rsocketStrategies = rsocketStrategies;
 	}
 
 	public void connect(String host, Integer port) {
         log.info("\nConnecting to server...");
-		this.rsocketRequester = rsocketRequesterBuilder.connectTcp(host, port).block(Duration.ofSeconds(10));
+        UsernamePasswordMetadata user = new UsernamePasswordMetadata("user", "pass");
+		this.rsocketRequester = rsocketRequesterBuilder
+				.setupMetadata(user, SIMPLE_AUTH)
+				.rsocketStrategies(builder-> builder.encoder(new SimpleAuthenticationEncoder()))
+				.connectTcp(host, port).block(Duration.ofSeconds(10));
         log.info("\nConnected to {}:{}", host, port);		
 	}
 	Availability connectionAvailability() {
